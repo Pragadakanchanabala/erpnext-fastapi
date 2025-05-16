9d2bb41cbf24ced8869ff2123bdfbfcb2c2d5a1b
Pragadakanchanabala <pragadakanchan@gmail.com>
2025-05-16 19:44:08 +0530

    tasks are included in main

--- database.py
+++ database.py
@@ -5,11 +5,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 load_dotenv()
 
 MONGO_URI="mongodb+srv://pragadakanchana:Kanchana01p@cluster0.d2nmvpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URI="mongodb+srv://<username>:<password>@cluster0.d2nmvpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
 client = AsyncIOMotorClient(MONGO_URI)
 
 db = client.kisanmitra

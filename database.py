7879831c6bade09e618b18e09b0aaec2cd27392d
Pragadakanchanabala <pragadakanchan@gmail.com>
2025-05-16 19:32:11 +0530

    Resolve merge conflict in database.py

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -6,12 +5,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 load_dotenv()
 
 MONGO_URI="mongodb+srv://pragadakanchana:Kanchana01p@cluster0.d2nmvpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-=======
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URI="mongodb+srv://<username>:<password>@cluster0.d2nmvpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
->>>>>>> efdb084018bed60b255789dd4ab79a365e276a72
 
 client = AsyncIOMotorClient(MONGO_URI)
 
